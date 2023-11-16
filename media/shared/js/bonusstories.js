5e982b03541b01b30f7fec31b79316a1823a05a9
Dirk Jan Buter <djbuter@gmail.com>
2023-11-16 17:18:05 +0100

    qr-ver

--- media/shared/js/bonusstories.js
+++
@@ -1,7 +0,0 @@
-function accessBonusStoryContent(id, mdName)
-{
-	var content = document.getElementById(id);
-	var emailField = document.createElement("input");
-    	emailField.type = "email";
-	content.appendChild(emailField);
-}
