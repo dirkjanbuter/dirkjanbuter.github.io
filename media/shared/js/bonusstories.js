0010307864fb3722510331c05a79ef6ce698ff97
Dirk Jan Buter <djbuter@gmail.com>
2023-11-16 11:32:55 +0100

    media

---
+++ media/shared/js/bonusstories.js
@@ -0,0 +1,7 @@
+function accessBonusStoryContent(id, mdName)
+{
+	var content = document.getElementById(id);
+	var emailField = document.createElement("input");
+    	emailField.type = "email";
+	content.appendChild(emailField);
+}
